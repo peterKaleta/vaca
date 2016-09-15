8295188c30c661437ffb2765acd8e6df643daa26
Peter Kaleta <ext.pkaleta@riotgames.com>
2016-09-15 13:53:14 +0200

    foxes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 var uniqueRandomArray = require('unique-random-array');
-var cows = require('cows');
+var fs = require('fs');
+var path = require('path');
 
-module.exports = uniqueRandomArray(cows());
+var foxes = function () {
+	return fs.readFileSync(path.join(__dirname, 'foxes.txt'), 'utf8')
+		.replace(/\n$/, '').split('\n\n\n');
+};
+
+
+module.exports = uniqueRandomArray(foxes());
